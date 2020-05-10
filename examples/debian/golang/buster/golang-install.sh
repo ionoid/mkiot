ecde44aba0de6fb691b25b2f4c34141ab0b8cb4b
Djalal Harouni <tixxdz@opendevices.io>
2020-05-10 01:11:08 +0100

    golang: improve the golang install script
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

--- examples/debian/golang/buster/golang-install.sh
+++ examples/debian/golang/buster/golang-install.sh
@@ -4,17 +4,25 @@ url="https://golang.org/dl/go${GOLANG_VERSION}.${OS}-${GOLANG_ARCH}.tar.gz"
 
 # remove old go in case
 rm -fr /usr/local/go
+rm -f /usr/local/bin/go
+rm -f /usr/local/bin/gofmt
 
 wget -O go.tgz "$url"
 tar -C /usr/local -xzf go.tgz
 rm go.tgz
-export PATH="/usr/local/go/bin:$PATH"; \
+
+export PATH="/usr/local/go/bin:$PATH"
+
+# Lets creat go links in /usr/local/bin/ to make it easy for developers
+cpwd=$(pwd)
+cd /usr/local/bin/
+ln -sr ../go/bin/go go > /dev/null 2>&1
+ln -sr ../go/bin/gofmt gofmt > /dev/null 2>&1
+cd $cpwd
+
 go version
 
 mkdir -p "${GOPATH}"
-echo "export PATH=$PATH:/usr/local/go/bin" > /etc/profile.d/gopath.sh
-echo "export GOPATH=${GOPATH}" >> /etc/profile.d/gopath.sh
-
-mkdir -p "$GOPATH/src"
-mkdir -p "$GOPATH/bin"
-chmod -R 755 "$GOPATH"
+mkdir -p "${GOPATH}/src"
+mkdir -p "${GOPATH}/bin"
+chmod -R 755 "${GOPATH}"

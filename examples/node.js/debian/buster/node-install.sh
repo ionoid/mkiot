dddb7987b897b9e319299d9a8c7b1d5ab5d1b6d1
Djalal Harouni <tixxdz@opendevices.io>
2020-06-10 11:36:23 +0100

    examples: add node.js based on minimal debian
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

---
+++ examples/node.js/debian/buster/node-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+# Original source form: https://github.com/nodesource/distributions/blob/master/README.md
+
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs

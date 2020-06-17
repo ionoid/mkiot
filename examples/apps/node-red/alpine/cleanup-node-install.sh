0332a4126fafafa8b473bf09fb732e6971a95614
Djalal Harouni <tixxdz@opendevices.io>
2020-06-17 20:31:21 +0100

    example: update node-red buildspec
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

--- examples/apps/node-red/alpine/cleanup-node-install.sh
+++ examples/apps/node-red/alpine/cleanup-node-install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 rm -fr /var/cache/apk/* && rm -fr /tmp/*
+rm -fr /usr/share/man/* /usr/share/doc /root/.npm /root/.node-gyp /root/.config \
+        /usr/lib/node_modules/npm/man /usr/lib/node_modules/npm/doc /usr/lib/node_modules/npm/docs \
+        /usr/lib/node_modules/npm/html
+
+# rm -fr /usr/lib/node_modules/npm/scripts

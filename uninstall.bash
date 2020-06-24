9e46b1f07a6a4b7f679cd17347a2c472595f04e9
Djalal Harouni <tixxdz@opendevices.io>
2020-06-24 09:07:57 +0100

    uninstall: update default cache path
    Signed-off-by: Djalal Harouni <tixxdz@opendevices.io>

--- uninstall.bash
+++ uninstall.bash
@@ -4,10 +4,10 @@ set -e
 
 main() {
         local INSTALL_DIR="/usr/lib/mkiot/"
-        local MKIOT_LIB="/var/lib/mkiot/"
+        local MKIOT_CACHE="/var/cache/mkiot/"
 
         rm -fr "${INSTALL_DIR}"
-        rm -fr "${MKIOT_LIB}"
+        rm -fr "${MKIOT_CACHE}"
         rm -f "/usr/bin/mkiot"
         rm -f "/usr/bin/mkiot.bash"
 
